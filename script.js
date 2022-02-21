4a0b51410fd25b0f2f246041e3058621f2f8a6d7
rd1919 <msocialonly@gmail.com>
2022-02-21 12:28:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle input'); 
+const header = document.querySelector('header ul');
+
+menuToggle.addEventListener('click', function(){
+  header.classList.toggle('slide');
+}) 
